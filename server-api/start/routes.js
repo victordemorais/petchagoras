27fb92eca6964e0775d2a904299e7e637d5dc3a7
Victor de Morais <victor@2x3.com.br>
2019-04-16 10:55:10 -0300

    Adicionado na api a alteração de senha do usuário com expiração de token, ou seja após dois dias que o usuário solicitou a alteração de senha ele não poderá resetar a senha.

--- server-api/start/routes.js
+++ server-api/start/routes.js
@@ -4,3 +4,4 @@ Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 
 Route.post('forgotpassword', 'ForgotPasswordController.store')
+Route.put('forgotpassword', 'ForgotPasswordController.update')

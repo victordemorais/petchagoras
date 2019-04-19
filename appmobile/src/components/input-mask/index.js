2decc41c6446cbec93d34bc75d777e2da5978273
Mateus <47222289+mteusgsouza@users.noreply.github.com>
2019-04-19 15:10:38 -0300

    comentários

--- appmobile/src/components/input-mask/index.js
+++ appmobile/src/components/input-mask/index.js
@@ -3,6 +3,13 @@ import { StyleSheet, View, Text } from "react-native"
 import TextInputMask from 'react-native-text-input-mask';
 
 
+/*click+ctrl no TextInputMask abre as a classe TextInputMask, onde é possivel visualizar as propriedades que podem ser utilizadas.
+Para adicionar uma prorpriedade de TextInputMask informe o nome da propriedade, e ela deve receber um props da classe que a utilizar
+
+exemplo:
+onChangeText={props.onChangeText}
+a pripriedade onChangeText de outra classe é recebida como onChangeText aqui.
+*/
 
 const InputMask = (props) => {
    
@@ -14,11 +21,11 @@ const InputMask = (props) => {
             refInput={props.refInput}
             onSubmitEditing={props.onSubmitEditing}
 
-            /*onChangeText={(formatted, extracted) => {
-                console.log(formatted) // exemplo +1 (123) 456-78-90 mask= +1 ([000]) [000] [00] [00]
-                console.log(extracted) // exemplo 1234567890
-            }}*/
-           
+            onChangeText={(formatted, extracted) => {
+                /*console.log(formatted) // texto formatado - exemplo +1 (123) 456-78-90 mask= +1 ([000]) [000] [00] [00]
+                console.log(extracted) // texto extraido - exemplo 1234567890*/
+            }}
+            onChangeText={props.onChangeText}
             mask={props.mask}
             placeholder={props.placeholder}
             maxLength={props.maxLength}
